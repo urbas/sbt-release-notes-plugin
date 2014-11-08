b2a9da0853f8fdfcf0f54bf1db18337de9788bf9
Matej Urbas <matej.urbas@gmail.com>
2014-11-08 16:58:05 +0000

    Fixed tests for the root folder strategy.

--- releaseNotesPlugin/src/sbt-test/root-folder-release-notes/simple/project/BuildConfiguration.scala
+++ releaseNotesPlugin/src/sbt-test/root-folder-release-notes/simple/project/BuildConfiguration.scala
@@ -1,6 +1,6 @@
 import sbt._
-import si.urbas.sbt.releasenotes.MdReleaseNotesFormat
-import si.urbas.sbt.releasenotes.test.ReleaseNotesSelfTestPlugin
+import si.urbas.sbt.releasenotes._
+import si.urbas.sbt.releasenotes.test._
 
 object BuildConfiguration extends Build {
   val root = project.in(file("."))
