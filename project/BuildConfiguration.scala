2ca9758a05510f8367e3813e4b446cd4ca18ec40
Matej Urbas <matej.urbas@gmail.com>
2014-11-22 10:20:23 +0000

    Using the previous release of the release notes plugin.

--- project/BuildConfiguration.scala
+++ project/BuildConfiguration.scala
@@ -6,7 +6,6 @@ import sbtrelease.ReleasePlugin._
 import sbtrelease.ReleaseStateTransformations._
 import sbtrelease._
 import si.urbas.sbt.releasenotes.ReleaseNotesPlugin._
-import si.urbas.sbt.releasenotes.strategies._
 import xerial.sbt.Sonatype.SonatypeKeys._
 import xerial.sbt.Sonatype.sonatypeSettings
 
@@ -23,7 +22,6 @@ object BuildConfiguration extends Build {
     .aggregate(releaseNotesPlugin)
     .settings(PublishConfiguration.rootSettings: _*)
     .settings(ReleaseConfiguration.rootSettings: _*)
-    .enablePlugins(GitHubReleaseNotesStrategy)
 
   lazy val releaseNotesPlugin = project.in(file("releaseNotesPlugin"))
     .settings(scriptedSettings ++ sonatypeSettings ++ releaseSettings ++ PublishConfiguration.disableDocPublish: _*)
